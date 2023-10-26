bfb8ab639ece40c52e8beb4a333db84cb5906681
WarrenHawker <hawker.warren@gmail.com>
2023-10-26 11:10:49 +0100

    added increased connection timeout for redis client

--- src/lib/redis/redisClient.ts
+++ src/lib/redis/redisClient.ts
@@ -6,6 +6,9 @@ import 'dotenv/config';
 //initialise redis client
 export const redisClient = createClient({
   url: process.env.REDIS_URL || '',
+  socket: {
+    connectTimeout: 50000,
+  },
 });
 
 //initialise redis store
