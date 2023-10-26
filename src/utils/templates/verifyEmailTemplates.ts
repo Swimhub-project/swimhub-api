dc35e83120308108fd608f0e817bc5cf8b033a37
WarrenHawker <hawker.warren@gmail.com>
2023-10-26 11:18:36 +0100

    fixed base url string again

--- src/utils/templates/verifyEmailTemplates.ts
+++ src/utils/templates/verifyEmailTemplates.ts
@@ -1,5 +1,5 @@
 // const baseUrl = process.env.BASE_URL || 'http://localhost:5000';
-const baseUrl = 'https://swimhub-production.up.railway.app/';
+const baseUrl = 'https://swimhub-production.up.railway.app';
 
 export const verifyEmailText = (name: string, id: string, token: string) => {
   return `Hi ${name}. Thank you for joining Swimhub. 
